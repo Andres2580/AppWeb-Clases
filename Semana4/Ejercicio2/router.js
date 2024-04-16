09dc929d32747e9d3443262f23126ab5bfdd8cc8
Andres2580 <143357014+Andres2580@users.noreply.github.com>
2024-04-16 18:43:58 -0500

    Create router.js

---
+++ Semana4/Ejercicio2/router.js
@@ -0,0 +1,18 @@
+import { createWebHistory, createRouter } from 'vue-router'
+import CreateUserComponent from '@/components/create-user.component.vue'
+import MainLoginComponent from '@/components/main-login.component.vue'
+import ListUserComponent from '@/components/list-user.component.vue'
+
+const routes = [
+  { path: '/', component: MainLoginComponent },
+  { path: '/users', component: ListUserComponent },
+  { path: '/create-user', component: CreateUserComponent },
+
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+export default router;
