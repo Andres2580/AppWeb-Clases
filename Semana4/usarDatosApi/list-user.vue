ce13c7d564e30cdfadf6fb151b0a40eed5e6e125
Andres2580 <143357014+Andres2580@users.noreply.github.com>
2024-04-16 22:37:54 -0500

    Create list-user.vue

---
+++ Semana4/usarDatosApi/list-user.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+  <pv-button>Crear usuario</pv-button>
+  <ul v-for="user in users">
+    <li>{{ user.id + ' - ' + user.name }}</li>
+    <router-link :to="{ name: 'updateUser', params: { id: user.id } }">Edit</router-link>
+  </ul>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: 'list-user',
+  data() {
+    return {
+      users: []
+    }
+  },
+  async created() {
+    //this.users = await axios.get("http://localhost:3000/users")
+    
+    axios.get("http://localhost:3000/users").then((response)=>
+      {
+        console.log('users',response.data)
+        this.users = response.data;
+      }
+    )
+  }
+}
+</script>
+
+<style scoped></style>
