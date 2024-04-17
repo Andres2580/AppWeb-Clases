1a04018232d58a3a75bbb7f1d9e897597dc5910c
Andres2580 <143357014+Andres2580@users.noreply.github.com>
2024-04-16 23:10:16 -0500

    Create listUser.vue

---
+++ Semana4/usarDatosApi/Desacolplar/listUser.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+  <pv-button>Crear usuario</pv-button>
+  <ul v-for="user in users">
+    <li>{{ user.id + ' - ' + user.name }}</li>
+    <router-link :to="{ name: 'updateUser', params: { id: user.id } }">Edit</router-link>
+  </ul>
+  </div>
+</template>
+
+<script>
+import {UserApiServices} from '@/services/user-api.services.js';
+
+export default {
+  name: 'list-user',
+  data() {
+    return {
+      users: [],
+        userService : new UserApiServices()
+    }
+  },
+  async created() {
+    
+    this.userService.getAll().then((response)=>
+      {
+        console.log('users',response.data)
+        this.users = response.data
+      }
+    )
+  }
+}
+</script>
+
+<style scoped></style>
