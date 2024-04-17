ad63a72c88236ccb79b09272ae93084e8211c977
Andres2580 <143357014+Andres2580@users.noreply.github.com>
2024-04-16 21:47:31 -0500

    Create main.js

---
+++ Semana4/Ejercicio3-Bottons/main.js
@@ -0,0 +1,20 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import PrimeVue from 'primevue/config'
+import 'primevue/resources/themes/aura-light-green/theme.css'
+import Button from 'primevue/button'
+import InputText from 'primevue/inputtext'
+import router from '@/router.js'
+import i18n from '@/locale/i18n.js'
+
+const app = createApp(App)
+app.use(PrimeVue)
+app.use(i18n)
+app.use(router)
+
+app.component('pv-button', Button)
+app.component('pv-input-text', InputText)
+
+app.mount('#app') 
